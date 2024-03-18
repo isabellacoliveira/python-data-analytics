903b5c6a3d7afd31801ea644143b972ddeeca80c
Isabella Oliveira <ioliveira@iportsolutions.com.br>
2024-03-18 11:35:40 -0300

    feat: training conversions

---
+++ Desktop/estudos-alura/python-bootcamp/conversao.py
@@ -0,0 +1,28 @@
+# do tipo float para int 
+print(int(1.0))
+
+# string para int 
+print(int("1"))
+
+# string para float 
+print(float("10.10"))
+
+# int para float 
+print(float(100))
+
+# float para string 
+# no terminal, não conseguimos perceber o tipo especificamente, pois ele não vem entre aspas
+print(str(10.10))
+# perguntar qual é a classe tipo daquela variável 
+print(type(str(10.10)))
+
+valor = 90 
+valor_str = str(valor)
+print(type(valor)) # int 
+print(type(valor_str)) # string
+
+# dividir número inteiro por outro número inteiro 
+print(100 / 2)
+print(100 // 2) # retorna com a conversão 
+
+
---
+++ Desktop/estudos-alura/python-bootcamp/variaveis_e_constantes.py
@@ -0,0 +1,13 @@
+nome = "Isabella"
+idade = 19
+
+nome = "Alex"
+
+print(nome, idade)
+
+limite_estoque_diario = 1000
+
+# constantes 
+BRAZILIAN_STATES = ["SP", "RJ", "SC"]
+print(BRAZILIAN_STATES)
+
