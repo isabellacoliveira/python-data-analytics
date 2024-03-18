8408ffcb2cfe750908b3ce7ff8739688ea1f6dcc
Isabella Oliveira <ioliveira@iportsolutions.com.br>
2024-03-18 12:01:41 -0300

    feat: interacting with user

---
+++ Desktop/estudos-alura/python-bootcamp/print_input.py
@@ -0,0 +1,9 @@
+nome = input("Informe o seu nome: ")
+idade = input("Informe a sua idade: ")
+
+print(nome)
+print(idade)
+print("teste", end=" ")
+print(nome, idade, end="...\n")
+print(nome, idade, sep="#" end="...\n")
+print(nome, idade, sep="#")
