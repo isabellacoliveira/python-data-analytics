d15c93feb1f50c875bb3352d093612a332fcf001
Isabella Oliveira <ioliveira@iportsolutions.com.br>
2024-03-21 11:05:28 -0300

    feat: learning functions

---
+++ Desktop/estudos-alura/python-bootcamp/identacao_bloco.py
@@ -0,0 +1,17 @@
+# def representa a criação de uma função 
+def sacar(valor: float):
+    saldo = 500
+
+    # sabe que o print está dentro do bloco if devido a identação
+    if saldo >= valor: 
+        print("valor sacado")
+        print("retire o seu dinheiro na boca do caixa")
+    
+    print("obrigado por ser nosso cliente, tenha um bom dia")
+
+def depositar(valor):
+    saldo = 500 
+    saldo >= valor
+
+sacar(100)
+depositar(10000)
